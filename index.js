e5fd6428c5a7ec17bd3773a023ff11630116ef0c
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2025-01-05 10:24:22 +0700

    Figured out how to do module

--- index.js
+++ index.js
@@ -1,55 +1,44 @@
-const express = require('express'),
-    http = require('http'),
-    WebSocket = require('ws'),
-    path = require('path');
+import express from 'express';
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+import path from 'path';
 
 const app = express(),
-    server = http.createServer(app),
-    wss = new WebSocket.Server({ server });
+    server = createServer(app),
+    wss = new WebSocketServer({ server });
 
-// game code
-class Plane {
-    constructor(x, y, hdg, spd, alt) {
-        this.x = x;
-        this.y = y;
-        this.hdg = hdg;
-        this.spd = spd;
-        this.alt = alt;
-    }
-    tick() {
-        this.x += this.spd * Math.cos(this.hdg);
-        this.y += this.spd * Math.sin(this.hdg);
-    }
+const __dirname = path.resolve();
 
-}
+console.log(__dirname)
+
+import Game from './client/game.js'
 
 // Handle HTTP requests
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use('/client', express.static(path.join(__dirname, 'client')));
 
 // Handle WebSocket connections
 let CLIENTS = [];
 
 function sendExceptCurrent(client, message) {
-    for (var i = 0; i < CLIENTS.length; i++) {
-        if (CLIENTS[i] == client) continue;
+    for (let i = 0; i < CLIENTS.length; i++) {
+        if (CLIENTS[i] === client) continue;
 
         CLIENTS[i].send(message);
     }
 }
 
 function sendAll(message) {
-    for (var i = 0; i < CLIENTS.length; i++) {
+    for (let i = 0; i < CLIENTS.length; i++) {
         CLIENTS[i].send(message);
     }
 }
 
 wss.on('connection', (ws) => {
     CLIENTS.push(ws);
-
 
     ws.on('message', (message) => {
         console.log('Received:', message);
